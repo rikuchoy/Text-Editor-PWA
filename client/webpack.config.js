ceaa74bf57c518b6e01a5bd405899412faf09866
Riku Choy <rikuchoy@gmail.com>
2023-08-21 17:09:15 -0700

    added plugins and loaders to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,19 +12,58 @@ module.exports = () => {
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
+      database: './src/js/database.js'
+      eidtor: './src/js/editor.js'
+      header: './src/js/header.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A simple text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        fingerprints: false,
+        inject: true,
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'), 
+          },
+        ],
+      }),
     ],
+
 
     module: {
       rules: [
-        
-      ],
-    },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env"],
+                plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              },
+            },
+          },
+        ],              
+      },
+    };
   };
-};
